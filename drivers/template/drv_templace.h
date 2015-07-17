07d73f4b659a23ff51242c12ed671de1acb340f9
Jan <j.dolinay@email.cz>
2015-07-17 14:53:08 +0200

    Added template files for a driver.

---
+++ drivers/template/drv_templace.h
@@ -0,0 +1,51 @@
+/*
+ * @file     drv_.h
+ * @brief     driver for Freescale KL25Z
+ * @version  V1.00
+ * @date     17. July 2015
+ *
+ * @note
+ *
+ * Ukazkovy program pro Programovani mikropocitacu
+ * Ovladac pro .
+ *
+ * Pouzite piny:
+ * TODO:
+ *
+ */
+#ifndef	UTBFRDM_DRV_ name _H
+#define UTBFRDM_DRV_ name _H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/** \ingroup  UTB_FRDM_Drivers
+    \defgroup UTB_FRDM_NewDriver New Driver for FRDM-KL25Z
+  @{
+ */
+
+
+ /**
+  * @brief Initialize the
+  * @param param Must be one of the values defined in UART0_baudrate enum!
+  * @return none
+  * @note
+  *
+  */
+void todo_initialize(param);
+
+
+
+
+
+/*@} end of UTB_FRDM_NewDriver */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* UTBFRDM_DRV_ name _H */
+
---
+++ drivers/template/drv_template.c
@@ -0,0 +1,37 @@
+/*
+ * Ukazkovy program pro Programovani mikropocitacu
+ * Ovladac pro .
+ *
+ * Detailni dokumentace je v .h souboru.
+ *
+ */
+
+#include "MKL25Z4.h"
+#include "drv_.h"
+
+
+
+/* Example Internal functions */
+/*static void _setbaudrate(UART0_baudrate baudrate);*/
+
+/* Example of internal constants*/
+/*const char UTB_UART_CR = (const char)0x0D;*/
+
+
+/*
+ * example function
+ */
+void todo_initialize(param)
+{
+
+
+}
+
+
+
+ /*internal function */
+/*
+static void uart0_setbaudrate(UART0_baudrate baudrate)
+{
+}
+*/
