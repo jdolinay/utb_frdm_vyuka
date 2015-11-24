2fb6017fbba315523cdc48c2854461b7dac7d23e
jdolinay <j.dolinay@email.cz>
2015-11-24 16:41:13 +0100

    Started demo project for FM tuner with I2C driver.

---
+++ programs/model_fm_tuner/model_fm_tuner.c
@@ -0,0 +1,30 @@
+/*
+ Ukazka prace s modulem FM radioprijimac
+
+ Vyuziva ovladac I2C dle CMSIS
+ Postup vytvoreni projektu s ovladacem I2C
+ *
+  */
+
+#include "MKL25Z4.h"
+#include "RTE_Device.h"
+
+
+static int i = 0;
+
+int main(void)
+{
+
+    /* Write your code here */
+
+
+
+    for (;;) {
+        i++;
+    }
+    /* Never leave main */
+    return 0;
+}
+////////////////////////////////////////////////////////////////////////////////
+// EOF
+////////////////////////////////////////////////////////////////////////////////
