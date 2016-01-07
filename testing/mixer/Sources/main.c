30b46757d9e8c6a8a0c10ed5269a0d3431a5c7f5
jdolinay <j.dolinay@email.cz>
2016-01-07 13:05:42 +0100

    test project for mixer edumod model updated - added option for controlling via cmd line via UART (OpenSDA serial port).

--- testing/mixer/Sources/main.c
+++ testing/mixer/Sources/main.c
@@ -1,8 +1,27 @@
 /*
  * Projekt pro testovani modelu Misici jednotky EDU MOD
+ * Verze 1 umoznuje ovladani pres seriovou linku, ryhclost 9600 bd
+ * prikazy jako "sv1on" a "sv1off" viz cliProcessCommand.
+ * Verze 2 je sekvenci napusteni a vypusteni. Aby se aktivovala verze 2
+ * je nutno zakomentovat volani verze 1 v main!
 */
 
 #include "MKL25Z4.h"
+#include "drv_uart.h"
+#include <stdbool.h>
+#include <string.h>     // pro funkci strcmp()
+
+
+// max length of a command
+#define   MAX_COMMAND_LEN       (7)
+
+// prototypes
+void cliProcessCommand(void);
+bool cliBuildCommand(char nextChar);
+
+// buffer for storing the command
+char G_CommandBuffer[MAX_COMMAND_LEN + 1];
+
 
 // Model vyuziva piny na portech B, C, D, E
 // Makra pro nastaveni vystupu
@@ -33,6 +52,7 @@
 
 // Prototypy funkci definovanych nize
 void delay(void);
+void cli_mixer(void);	// ovladani modelu pres UART
 
 
 
@@ -83,6 +103,12 @@ int main(void) {
 	PTC->PDDR &= ~(1 << 6);
 	PTC->PDDR &= ~(1 << 5);
 
+	/* Zakomentovanim cli_mixer() se pouzije verze 2 */
+
+	/* VERZE 1 s ovladanim pres seriovou linku*/
+	cli_mixer();
+
+	/* VERZE 2 s napustenim a vypustenim */
 	while(1) {
 
 		// nadrz 1
@@ -172,6 +198,173 @@ void delay(void)
 	while ( n-- )
 		;
 }
+
+/*
+ * ovladani modelu pres textove prikazy pres UART
+ * Zavolat z main a toto se pak stava hlavni smyckou programu.
+ * */
+void cli_mixer(void) {
+
+	char rcvChar;
+	bool commandReady;
+
+	// Inicializace serioveho rozhrani UART
+	UART0_Initialize(BD9600);
+
+	while (1) {
+		// Cekame na prichod znaku
+		rcvChar = UART0_getch();
+
+		// Odesleme znak zpet na seriovou linku (echo)
+		UART0_putch(rcvChar);
+
+		/* Build a new command. */
+		commandReady = cliBuildCommand(rcvChar);
+
+		/* Call the CLI command processing routine to verify the command entered
+		 * and execute the command; then output a new prompt. */
+		if (commandReady) {
+			commandReady = false;
+			cliProcessCommand();
+			UART0_putch('>');
+		}
+	}
+
+}
+
+/**********************************************************************
+ *
+ * Function:    cliProcessCommand
+ *
+ * Description: Look up the command in the command table. If the
+ *              command is found, call the command's function. If the
+ *              command is not found, output an error message.
+ *
+ * Notes:
+ *
+ * Returns:     None.
+ *
+ **********************************************************************/
+void cliProcessCommand(void)
+{
+	if (strcmp(G_CommandBuffer, "sv1on") == 0) {
+
+		SV1_ON();
+		UART0_puts("\n\rT1 on\n\r");
+
+	} else if (strcmp(G_CommandBuffer, "sv1off") == 0) {
+
+		SV1_OFF();
+		UART0_puts("\n\rT1 off\n\r");
+
+	} else if (strcmp(G_CommandBuffer, "sv2on") == 0) {
+
+		SV2_ON();
+		UART0_puts("\n\rT2 on\n\r");
+
+	} else if (strcmp(G_CommandBuffer, "sv2off") == 0) {
+
+		SV2_OFF();
+		UART0_puts("\n\rT2 off\n\r");
+
+	} else if (strcmp(G_CommandBuffer, "sv3on") == 0) {
+
+		SV3_ON();
+		UART0_puts("\n\rT3 on\n\r");
+
+	} else if (strcmp(G_CommandBuffer, "sv3off") == 0) {
+
+		SV3_OFF();
+		UART0_puts("\n\rT3 off\n\r");
+
+	} else if (strcmp(G_CommandBuffer, "sv4on") == 0) {
+
+		SV4_ON();
+		UART0_puts("\n\rSV4 on\n\r");
+
+	} else if (strcmp(G_CommandBuffer, "sv4off") == 0) {
+
+		SV4_OFF();
+		UART0_puts("\n\rSV4 off\n\r");
+
+	} else if (strcmp(G_CommandBuffer, "sv5on") == 0) {
+
+		SV5_ON();
+		UART0_puts("\n\rSV5 on\n\r");
+
+	} else if (strcmp(G_CommandBuffer, "sv5off") == 0) {
+
+		SV5_OFF();
+		UART0_puts("\n\rSV5 off\n\r");
+
+	} else if (strcmp(G_CommandBuffer, "mixon") == 0) {
+
+		MICHADLO_ON();
+		UART0_puts("\n\rMix on\n\r");
+
+	} else if (strcmp(G_CommandBuffer, "mixoff") == 0) {
+
+		MICHADLO_OFF();
+		UART0_puts("\n\rMix off\n\r");
+
+	} else if (strcmp(G_CommandBuffer, "ver") == 0) {
+		/* send our version info */
+		UART0_puts("\n\rMixer tester version 1.0\n\r");
+
+	} else {
+		UART0_puts("\n\rUnknown command\n\r");
+	}
+
+}
+
+
+
+/**********************************************************************
+ *
+ * Function:    cliBuildCommand
+ *
+ * Description: Put received characters into the command buffer. Once
+ *              a complete command is received return 1.
+ *
+ * Notes:
+ *
+ * Returns:     1 if a command is complete, otherwise 0.
+ *
+ **********************************************************************/
+bool cliBuildCommand(char nextChar)
+{
+    static char idx = 0;
+
+    /* Don't store any new line characters or spaces. */
+    if ((nextChar == '\n') || (nextChar == ' ') || (nextChar == '\t'))
+        return 0;
+    /* The completed command has been received. Replace the final carriage
+     * return character with a NULL character to help with processing the
+     * command. */
+    if (nextChar == '\r')
+    {
+        G_CommandBuffer[idx] = '\0';
+        idx = 0;
+        return true;
+    }
+
+    /* Store the received character in the command buffer. */
+    G_CommandBuffer[idx] = nextChar;
+    idx++;
+
+    /* If the command is too long, reset the index and process
+     * the current command buffer. */
+    if (idx > MAX_COMMAND_LEN)
+    {
+        idx = 0;
+        return 1;
+    }
+
+    return false;
+}
+
+
+
 ////////////////////////////////////////////////////////////////////////////////
 // EOF
 ////////////////////////////////////////////////////////////////////////////////
