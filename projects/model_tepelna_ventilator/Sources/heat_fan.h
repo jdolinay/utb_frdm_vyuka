3380863529ced7ac34c207fcbafea49eeca20350
dolinay <j.dolinay@email.cz>
2017-11-23 14:46:05 +0100

    Added example project for heating plant with fan.

---
+++ projects/model_tepelna_ventilator/Sources/heat_fan.h
@@ -0,0 +1,22 @@
+/*
+ * heatfan.h.h
+ *
+ * Driver for heating plant with fan which connects to UTB learning kit
+ * with FRDM kl25z
+ */
+
+#ifndef SOURCES_HEATFAN_H_
+#define SOURCES_HEATFAN_H_
+
+typedef enum {
+	HEATFAN_Heater, HEATFAN_Fan
+} HEATFAN_Actuator;
+
+void HEATFAN_Init(void);
+int HEATFAN_GetTemperature(void);
+uint16_t HEATFAN_GetFanRPM(void);
+void HEATFAN_CtrlSignalSel(HEATFAN_Actuator vyber);
+void HEATFAN_DoPWMPulse(int pwm_percents);
+
+
+#endif /* SOURCES_HEATFAN_H_ */
